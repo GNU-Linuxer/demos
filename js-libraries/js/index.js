17485cd2864a347f16f61a5d0fbef5e037386b25
Joel Ross <joelross@uw.edu>
2020-05-03 21:35:19 -0700

    End of libraries demo

---
+++ js-libraries/js/index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+//let example = .... //in example_library.js
+// example.foo();
+// example.bar("world");
+
+// let content = "## Subheading \n\n I think `code` is **awesome**"
+
+// let htmlVersion = marked(content); //run it through the marked processor
+// console.log(htmlVersion);
+
+// document.querySelector('#content').innerHTML = htmlVersion;
+
+// let now = moment();
+// console.log(now.format("MM/DD"));
+
+// let stayAtHomeStart = moment("2020-03-25");
+// console.log(stayAtHomeStart.fromNow())
+
+//document.querySelector
+//document.createElement
+//document.appendChild
+
+//document.querySelector("#content")
+
+let contentElem = $("p")
+contentElem.text("This is new content!");
+contentElem.css('background-color', 'skyblue');
+
+$('main').append("<a href='google.com'>Go to Google</a>")
+console.log(contentElem);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js-libraries/lib/example_library.js
@@ -0,0 +1,4 @@
+const example = {
+    foo: function() { console.log("FOO!!!")},
+    bar: function(arg) {console.log("Bar "+arg)}
+}
